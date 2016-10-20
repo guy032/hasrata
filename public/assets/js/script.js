45683987340d7102e65ca0a3c62771715fc33fa6
guy032 <guy032@gmail.com>
2016-10-20 12:57:21 +0300

    changes page 3 and 4

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,6 +7,7 @@ var config = {
 };
 firebase.initializeApp(config);
 var storageRef = firebase.storage().ref();
+var debug;
 
 $(document).ready(function() {
 	$(".buttons .button").click(function() {
@@ -49,15 +50,67 @@ $(document).ready(function() {
 		$('.months').append('<option value="' + i + '">' + months[i-1] + '</option>');
 	}
 
-	var year = new Date().getFullYear();
-	for(i = year; i >= year - 30; i--) {
-		$('.years').append('<option value="' + i + '">' + i + '</option>');
-	}
+	$('.years').append(getYearsOptions())
 
 	var citizenships = ["אוקראינית","איטלקית","אנגלית","אפריקנס","בולגרית","גרמנית","הונגרית","הינדי","יידיש","נורווגית","סינית","סלובקית","ספרדית","עברית","ערבית","פולנית","פורטוגזית","צרפתית","רומנית","רוסית","אמהרית","ארמנית","גאורגית","דנית","הולנדית","יוונית","יפנית","לאדינו","לטבית","לטינית","ליטאית","מנדרינית","סרבית","פלמית","פרסית","צ'כית","קרואטית","שוודית"];
 	for(i = 1; i <= citizenships.length; i++) {
 		$('.citizenships').append('<option value="' + i + '">' + citizenships[i-1] + '</option>');
 	}
+
+    $("input[type='checkbox']").change(function() {
+		if(this.checked) {
+			$("." + $(this).attr('id')).removeAttr("disabled");
+		}
+		else {
+			$("." + $(this).attr('id')).attr("disabled", "");
+		}
+    })
+
+	$("#file-upload2").change(function(e) {
+		var files = this.files;
+		for (var i = 0; i < files.length; i++) {
+			element = '<tr class="portfolioFile">' +
+			    		'<td class="remove"><img class="plusRemoveIcon remove" onclick="AddRow()" src="assets/img/remove.png"></td>' +
+			    		'<td>'+files[i].name+'</td>' +
+					    '<td>' +
+					        '<select>' +
+					            '<option>צילום וידאו</option>' +
+					            '<option>צילום סטילס</option>' +
+					            '<option>תסריט ותחקיר</option>' +
+					            '<option>הפקה - תיק הפקה</option>' +
+					            '<option>הפקה - קובץ וידאו</option>' +
+					            '<option>עריכת וידאו</option>' +
+					            '<option>גרפיקה</option>' +
+					            '<option>אנימציה</option>' +
+					        '</select>' +
+					    '</td>' +
+					    '<td><input /></td>' +
+					    '<td><input /></td>' +
+					'</tr>'
+			$(".files_form3").append(element)
+		}
+	});
+
+	$(document).on('click', '.files_form3 .remove', function() {
+		$(this).closest('tr:not(.th)').remove()
+	})
+
+	var limit = 2;
+	$('input[name="2units"]').on('change', function(evt) {
+	   if($(this).siblings(':checked').length >= limit) {
+	       this.checked = false;
+	   }
+	});
+
+	$(document).on('click', '.addRemovePlugin .disabled .add', function() {
+		$(this).closest(".addRemovePlugin").append($(this).closest('.disabled').clone().addClass('new'))
+		$(this).closest('.addRemovePlugin').find('.disabled:not(.new)').removeClass('disabled').find(".plusRemoveIcon").addClass('remove').removeClass('add').attr('src', 'assets/img/remove.png')		
+		$(this).closest('.addRemovePlugin').find('.new').removeClass('new')
+	})
+
+	$(document).on('click', '.addRemovePlugin .remove', function() {
+		$(this).closest("tr").remove()
+	})
 })
 
 function getFileContent(input) {
@@ -67,17 +120,11 @@ function getFileContent(input) {
 	});
 }
 
-function AddRow() {
-	$(".disabledTemplate").clone().appendTo(".innerBox").removeClass("disabledTemplate");;
+function getYearsOptions() {
+	var year = new Date().getFullYear();
+	output = ''
+	for(i = year; i >= year - 30; i--) {
+		output += '<option value="' + i + '">' + i + '</option>';
+	}
+	return output
 }
-
-$(function() {
-    $("input[type='checkbox']").change(function() {
-		if(this.checked) {
-			$("." + $(this).attr('id')).removeAttr("disabled");
-		}
-		else {
-			$("." + $(this).attr('id')).attr("disabled", "");
-		}
-    })
-})
